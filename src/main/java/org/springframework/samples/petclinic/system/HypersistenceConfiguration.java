9f773a00b0f217da178720f15eafff96c1a17b2c
Vlad Mihalcea <mihalcea.vlad@gmail.com>
2019-11-04 14:07:11 +0200

    Configure the MySQL environement

--- src/main/java/org/springframework/samples/petclinic/system/HypersistenceConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/system/HypersistenceConfiguration.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.system;
 
 import io.hypersistence.optimizer.HypersistenceOptimizer;
 import io.hypersistence.optimizer.core.config.JpaConfig;
+import io.hypersistence.optimizer.hibernate.event.mapping.identifier.IdentityGeneratorEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,9 +15,10 @@ public class HypersistenceConfiguration {
     public HypersistenceOptimizer hypersistenceOptimizer(
             EntityManagerFactory entityManagerFactory) {
         return new HypersistenceOptimizer(
-            new JpaConfig(
-                entityManagerFactory
-            )
+            new JpaConfig(entityManagerFactory)
+                .setEventFilter(
+                    event -> !(event instanceof IdentityGeneratorEvent)
+                )
         );
     }
 }
--- src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
+++ src/test/java/org/springframework/samples/petclinic/PetclinicIntegrationTests.java
@@ -22,12 +22,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.samples.petclinic.vet.VetRepository;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("mysql")
 public class PetclinicIntegrationTests {
 
     @Autowired
